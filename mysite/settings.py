4eb3efdb971bd4cf3e96ee12503ba86c48fc072e
Dharu <168706043+patelDharu@users.noreply.github.com>
2025-09-16 21:55:15 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,12 +114,11 @@ USE_TZ = True
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # your dev folder
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
